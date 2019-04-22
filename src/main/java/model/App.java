bbdb8fee1b540099715a9dcd964324d2ac08173e
Bruno Vieira <bruno.vieira97@outlook.com>
2019-04-21 22:43:19 -0300

    Atualizada classe principal para que utilize classes Builder e Writer.

--- src/main/java/model/App.java
+++ src/main/java/model/App.java
@@ -1,11 +1,49 @@
 package model;
 
+import java.util.Scanner;
 
 public final class App {
+	private static final String
+		INPUT_FILENAME = System.getProperty("user.dir") + "\\resources\\PetriNet.txt",
+		OUTPUT_FILENAME = System.getProperty("user.dir") + "\\resources\\ExportedNetwork.txt";
+
     private App() {
     }
 
     public static void main(String[] args) {
-        System.out.println("Oh yes");
+		
+		Scanner scanner = new Scanner(System.in);
+		PetriNetworkBuilder builder = new PetriNetworkBuilder();
+
+		PetriNetwork network = builder.buildFromFile(INPUT_FILENAME);
+
+		System.out.println("Petri Network Simulator");
+		System.out.println("Pressione ENTER para executar os ciclos da simulação.");
+		System.out.println("Outras opções: ");
+		System.out.println("	S:	Salvar rede em arquivo.");
+
+		while (!network.EOF()) {
+			String input = scanner.nextLine();
+
+			int cycle = 0;
+
+			switch (input) {
+				case "S":
+					PetriNetworkWriter writer = new PetriNetworkWriter();
+
+					writer.saveToFile(network, OUTPUT_FILENAME);
+
+				break;
+
+				default:
+					network.printCurrentCycle(cycle);
+
+					cycle++;
+				break;
+			}
+		}
+
+		scanner.close();
+
     }
 }
